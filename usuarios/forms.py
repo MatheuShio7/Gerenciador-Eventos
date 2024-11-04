5cc5d458e876496ad6dfe2269903b3d32c4953b2
Matheus Shiokawa Silva <matheus.pedro84@hotmail.com>
2024-11-04 19:49:59 -0300

    18
    Front das páginas de login e cadastro, modificação dos requisitos de senha.

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 from .models import Usuario
 
 class UsuarioForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, label='Primeiro Nome')
+    last_name = forms.CharField(max_length=30, required=True, label='Último Nome')
     data_nascimento = forms.DateField(
         widget=forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d')
     )
@@ -12,13 +14,21 @@ class UsuarioForm(UserCreationForm):
 
     class Meta:
         model = Usuario
-        fields = ['username', 'email', 'data_nascimento', 'sexo']
+        fields = ['username', 'first_name', 'last_name', 'email', 'data_nascimento', 'sexo']
+        help_texts = {
+            'username': None,
+        }
 
     def clean_data_nascimento(self):
         data_nascimento = self.cleaned_data.get('data_nascimento')
         if data_nascimento >= timezone.now().date():
             raise ValidationError("A data de nascimento não pode ser no futuro.")
         return data_nascimento 
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Removendo a mensagem de ajuda do campo password2 (confirmação de senha)
+        self.fields['password2'].help_text = ''
 
 
 class EditarPerfilForm(forms.ModelForm):
